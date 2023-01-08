96da78b33d8016e4d9cfe488fac02d9ceadfeca6
Bruno Signori <signoribruno@gmail.com>
2023-01-08 12:43:09 -0600

    first commit

---
+++ app/server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+    if ("counter" in req.session) {
+        req.session["counter"] += 1;
+    } else {
+        req.session["counter"] = 0;
+    }
+    
+    res.render("index", { counter: req.session.counter});
+})
+
+app.get('/one', function(req, res) {
+
+    req.session["counter"] += 1;
+    
+    res.redirect("/");
+})
+
+app.get('/reset', function(req, res) {
+
+    req.session["counter"] = 0;
+    
+    res.redirect("/");
+})
+
+
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
