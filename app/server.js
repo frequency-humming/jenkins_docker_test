309df582c6be1960847e5f1e408db8e542006717
Bruno Signori <signoribruno@gmail.com>
2023-01-15 14:05:31 -0600

    adding session and pool

--- app/server.js
+++ app/server.js
@@ -3,38 +3,41 @@ var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-const {Client} = require('pg');
+const {Pool} = require('pg');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
 
-const client = new Client({
-    user:'admin',
-    host:'host.docker.internal',
-    database:'test_db',
-    password:'mypassword',
-    port:5432,
-});
+const db = async () => {
+    try {
+        const pool = new Pool ({
+            user:'admin',
+            host:'host.docker.internal',
+            database:'test_db',
+            password:'mypassword',
+            port:5432,
+        });
+        await pool.connect();
+        const res = await pool.query('SELECT * FROM testing');
+        console.log(res);
+        await pool.end();
+    } catch (error){
+        console.log(error);
+    }
+}
 
-client.connect().then(() => {
-    client.query('SELECT NOW()', (err,res) => {
-        console.log(res.rows)
-    });
-}).catch((err) => {
-    console.log(err)
-});
-
-app.get('/', function(req, res) {
-    client.query('SELECT * FROM testing', (err,res) => {
-        if(err){
-            console.log(err.stack)
-        } else {
-            console.log(res.rows[0])
-        }
-    });
+app.get('/', function(req, res){
+    console.log('in the method');
+    db();
     if ("counter" in req.session) {
         req.session["counter"] += 1;           
     } else {
