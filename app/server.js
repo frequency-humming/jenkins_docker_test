0009832a37fa2ddf398bacbb5f95104f2b2690e0
Bruno Signori <signoribruno@gmail.com>
2023-05-20 18:31:07 -0400

    first push

--- app/server.js
+++ app/server.js
@@ -1,68 +1,37 @@
 var express = require("express");
-var path = require("path");
-var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
-const {Pool} = require('pg');
+const { Client } = require('pg');
+const http = require('http');
+const cors = require('cors'); 
+const fetch = require('node-fetch');
 
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
 
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
+const client = new Client({
+  connectionString: `postgresql://${process.env.user}:${process.env.password}@${process.env.HOSTNAME}/test_db`,
+});
+  
+client.connect();
 
-app.use(session({
-    secret: 'test',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}));
+client.query('LISTEN salesforce');
 
-const db = async () => {
-    try {
-        const pool = new Pool ({
-            user:'admin',
-            host:'host.docker.internal',
-            database:'test_db',
-            password:'mypassword',
-            port:5432,
-        });
-        await pool.connect();
-        const res = await pool.query('SELECT * FROM testing');
-        console.log(res);
-        await pool.end();
-    } catch (error){
-        console.log(error);
+client.on('notification', (msg) => {
+  fetch(`http://${process.env.HOSTNAME}:8000/api/streaming`, {
+    method: 'POST',
+    body: JSON.stringify({ payload: msg.payload }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-}
+  })
+  .then(response => {
+    console.log('Response:', response);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+});
 
-app.get('/', function(req, res){
-    console.log('in the method');
-    db();
-    if ("counter" in req.session) {
-        req.session["counter"] += 1;           
-    } else {
-        req.session["counter"] = 0;
-    }
-    
-    res.render("index", { counter: req.session.counter});
-})
-
-app.get('/one', function(req, res) {
-
-    req.session["counter"] += 1;
-    
-    res.redirect("/");
-})
-
-app.get('/reset', function(req, res) {
-
-    req.session["counter"] = 0;
-    
-    res.redirect("/");
-})
-
-
-
-app.listen(8000, function() {
-    console.log("listening on port 8000");
+server.listen(8888, function() {
+    console.log("listening on port 8888");
 });
